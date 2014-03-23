cf25a02abfa3a9d8ce90ab7a128ae045000eeeb8
Jonathan <johnnysouza3@gmail.com>
2014-03-23 17:41:26 -0300

    Adicionado demais componentes

--- Compilador/src/br/com/furb/ui/CompilerInterface.java
+++ Compilador/src/br/com/furb/ui/CompilerInterface.java
@@ -12,6 +12,11 @@ import javax.swing.JPanel;
 import javax.swing.SwingConstants;
 import javax.swing.border.EmptyBorder;
 import javax.swing.border.LineBorder;
+import javax.swing.JTextArea;
+import javax.swing.JLabel;
+import java.awt.GridBagLayout;
+import java.awt.GridBagConstraints;
+import java.awt.Insets;
 
 @SuppressWarnings("serial")
 public class CompilerInterface extends JFrame {
@@ -44,65 +49,88 @@ public class CompilerInterface extends JFrame {
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
-		
-		JPanel panel = new JPanel();
-		panel.setBorder(new LineBorder(new Color(0, 0, 0)));
-		contentPane.add(panel, BorderLayout.NORTH);
-		panel.setLayout(new GridLayout(0, 9, 0, 0));
-		
+
+		JPanel panelCompilador = new JPanel();
+		contentPane.add(panelCompilador, BorderLayout.CENTER);
+		panelCompilador.setLayout(new BorderLayout(0, 0));
+
+		JPanel panelFerramentas = new JPanel();
+		panelCompilador.add(panelFerramentas, BorderLayout.NORTH);
+		panelFerramentas.setBorder(new LineBorder(new Color(0, 0, 0)));
+		panelFerramentas.setLayout(new GridLayout(0, 9, 0, 0));
+
 		JButton btnNovo = new JButton("novo [ctrl-n]");
 		btnNovo.setIcon(new ImageIcon(".\\.\\.\\.\\.\\.\\Images\\newFile.png"));
 		btnNovo.setHorizontalTextPosition(SwingConstants.CENTER);
 		btnNovo.setVerticalTextPosition(SwingConstants.BOTTOM);
-		panel.add(btnNovo);
-		
+		panelFerramentas.add(btnNovo);
+
 		JButton btnAbrir = new JButton("abrir [ctrl-a]");
 		btnAbrir.setIcon(new ImageIcon(".\\.\\.\\.\\.\\.\\Images\\openFile.png"));
 		btnAbrir.setHorizontalTextPosition(SwingConstants.CENTER);
 		btnAbrir.setVerticalTextPosition(SwingConstants.BOTTOM);
-		panel.add(btnAbrir);
-		
+		panelFerramentas.add(btnAbrir);
+
 		JButton btnSalvar = new JButton("salvar [ctrl-s]");
-		btnSalvar.setIcon(new ImageIcon(".\\.\\.\\.\\.\\.\\Images\\saveFile.png"));
+		btnSalvar.setIcon(new ImageIcon(
+				".\\.\\.\\.\\.\\.\\Images\\saveFile.png"));
 		btnSalvar.setHorizontalTextPosition(SwingConstants.CENTER);
 		btnSalvar.setVerticalTextPosition(SwingConstants.BOTTOM);
-		panel.add(btnSalvar);
-		
+		panelFerramentas.add(btnSalvar);
+
 		JButton btnCopiar = new JButton("copiar [ctrl-c]");
 		btnCopiar.setIcon(new ImageIcon(".\\.\\.\\.\\.\\.\\Images\\copy.png"));
 		btnCopiar.setHorizontalTextPosition(SwingConstants.CENTER);
 		btnCopiar.setVerticalTextPosition(SwingConstants.BOTTOM);
-		panel.add(btnCopiar);
-		
+		panelFerramentas.add(btnCopiar);
+
 		JButton btnColar = new JButton("colar [ctrl-v]");
 		btnColar.setIcon(new ImageIcon(".\\.\\.\\.\\.\\.\\Images\\paste.png"));
 		btnColar.setHorizontalTextPosition(SwingConstants.CENTER);
 		btnColar.setVerticalTextPosition(SwingConstants.BOTTOM);
-		panel.add(btnColar);
-		
+		panelFerramentas.add(btnColar);
+
 		JButton btnRecortar = new JButton("recortar [ctrl-x]");
 		btnRecortar.setIcon(new ImageIcon(".\\.\\.\\.\\.\\.\\Images\\cut.png"));
 		btnRecortar.setHorizontalTextPosition(SwingConstants.CENTER);
 		btnRecortar.setVerticalTextPosition(SwingConstants.BOTTOM);
-		panel.add(btnRecortar);
-		
+		panelFerramentas.add(btnRecortar);
+
 		JButton btnCompilar = new JButton("compilar [F8]");
-		btnCompilar.setIcon(new ImageIcon(".\\.\\.\\.\\.\\.\\Images\\compile.png"));
+		btnCompilar.setIcon(new ImageIcon(
+				".\\.\\.\\.\\.\\.\\Images\\compile.png"));
 		btnCompilar.setHorizontalTextPosition(SwingConstants.CENTER);
 		btnCompilar.setVerticalTextPosition(SwingConstants.BOTTOM);
-		panel.add(btnCompilar);
-		
+		panelFerramentas.add(btnCompilar);
+
 		JButton btnGerarCod = new JButton("gerar c\u00F3digo [F9]");
-//		btnGerarCod.setIcon(new ImageIcon(".\\.\\.\\.\\.\\.\\Images\\.png")); //TODO achar icone para gerar código
+		// TODO achar icone para gerar código
+		// btnGerarCod.setIcon(new ImageIcon(".\\.\\.\\.\\.\\.\\Images\\.png"));
 		btnGerarCod.setHorizontalTextPosition(SwingConstants.CENTER);
 		btnGerarCod.setVerticalTextPosition(SwingConstants.BOTTOM);
-		panel.add(btnGerarCod);
-		
+		panelFerramentas.add(btnGerarCod);
+
 		JButton btnEquipe = new JButton("equipe [F1]");
 		btnEquipe.setIcon(new ImageIcon(".\\.\\.\\.\\.\\.\\Images\\group.png"));
 		btnEquipe.setHorizontalTextPosition(SwingConstants.CENTER);
 		btnEquipe.setVerticalTextPosition(SwingConstants.BOTTOM);
-		panel.add(btnEquipe);
+		panelFerramentas.add(btnEquipe);
+
+		JLabel lblStatus = new JLabel("N\u00E3o modificado");
+		panelCompilador.add(lblStatus, BorderLayout.SOUTH);
+
+		JPanel panelCentral = new JPanel();
+		panelCompilador.add(panelCentral, BorderLayout.CENTER);
+		panelCentral.setLayout(new GridLayout(2, 1, 0, 0));
+
+		JTextArea textEditor = new JTextArea();
+		panelCentral.add(textEditor);
+		textEditor.setBorder(new NumberedBorder());
+
+		JTextArea textMsg = new JTextArea();
+		textMsg.setBorder(new LineBorder(new Color(0, 0, 0)));
+		panelCentral.add(textMsg);
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 	}
 
 }
