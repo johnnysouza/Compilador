048ff95ba5f61a32cecdd9532e00bd0ac8df9196
Jonathan <johnnysouza3@gmail.com>
2014-05-21 19:21:46 -0300

    Sintático inicial

---
+++ Compilador/src/br/com/furb/lexico/Semantico.java
@@ -0,0 +1,9 @@
+package br.com.furb.lexico;
+
+public class Semantico implements Constants
+{
+    public void executeAction(int action, Token token)	throws SemanticError
+    {
+        System.out.println("Ação #"+action+", Token: "+token);
+    }	
+}
