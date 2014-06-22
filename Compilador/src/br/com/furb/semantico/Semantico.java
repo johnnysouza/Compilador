ff29d4308659b18d5042e5467a83b9901a2e872a
Jonathan <johnnysouza3@gmail.com>
2014-06-22 14:23:18 -0300

    Feito correção na lingagem no gals e gerado código novamente; Ajuste de algumas ações; Realizado lançamento de algumas excessões semânticas; Criados métodos para implementação das ações faltantes

--- Compilador/src/br/com/furb/semantico/Semantico.java
+++ Compilador/src/br/com/furb/semantico/Semantico.java
@@ -80,6 +80,24 @@ public class Semantico implements Constants {
 		case 17:
 			acao_17();
 			break;
+		case 18:
+			acao_18();
+			break;
+		case 19:
+			acao_19();
+			break;
+		case 20:
+			acao_20();
+			break;
+		case 21:
+			acao_21();
+			break;
+		case 22:
+			acao_22();
+			break;
+		case 23:
+			acao_23();
+			break;
 		case 24:
 			acao_24(token);
 			break;
@@ -98,12 +116,26 @@ public class Semantico implements Constants {
 		case 29:
 			acao_29(token);
 			break;
+		case 30:
+			acao_30();
+			break;
+		case 31:
+			acao_31();
+			break;
+		case 32:
+			acao_32();
+			break;
+		case 33:
+			acao_33();
+			break;
+		case 34:
+			acao_34();
+			break;
+		case 35:
+			acao_35();
+			break;
 		}
 
-	}
-
-	private void acao_0(Token token) {
-		tabelaSimbolo.put(token.getLexeme(), "prog");
 	}
 
 	private void acao_01() {
@@ -121,38 +153,63 @@ public class Semantico implements Constants {
 	}
 
 	private void acao_02() {
-		pilhaTipo.pop();
-		pilhaTipo.pop();
+		String tipo1 = pilhaTipo.pop();
+		String tipo2 = pilhaTipo.pop();
 
-		empilhaFloat();
+		if (isFloat(tipo1) || isFloat(tipo2)) {
+			empilhaFloat();
+		} else {
+			empilhaInt();
+		}
 
 		codeAppend("sub");
 	}
 
 	private void acao_03() {
-		empilhaInt();
+		String tipo1 = pilhaTipo.pop();
+		String tipo2 = pilhaTipo.pop();
+
+		if (isFloat(tipo1) || isFloat(tipo2)) {
+			empilhaFloat();
+		} else {
+			empilhaInt();
+		}
+
 		codeAppend("mul");
 	}
 
-	private void acao_04() {
-		pilhaTipo.push(ETipo.FLOAT.toString());
-		codeAppend("div");
+	private void acao_04() throws SemanticError {
+		String tipo1 = pilhaTipo.pop();
+		String tipo2 = pilhaTipo.pop();
+
+		if (tipo1.equalsIgnoreCase(tipo2)) {
+			pilhaTipo.push(tipo1);
+			codeAppend("div");
+		} else {
+			throw new SemanticError("Tipos incompatíves para divisão");
+		}
 	}
 
 	private void acao_05(Token token) {
+		empilhaInt();
 		codeAppend("ldc.i8 " + token.getLexeme());
 	}
 
 	private void acao_06(Token token) {
+		empilhaFloat();
 		codeAppend("ldc.r8 " + token.getLexeme());
 	}
 
 	private void acao_07() {
-		codeAppend("ldc.i8 -1");
-		codeAppend("mul");
+		String tipo = pilhaTipo.peek();
+
+		if (isInt(tipo) || isFloat(tipo)) {
+			codeAppend("ldc.i8 -1");
+			codeAppend("mul");
+		}
 	}
 
-	private void acao_08() {
+	private void acao_08() throws SemanticError {
 		String tipo1 = pilhaTipo.pop();
 		String tipo2 = pilhaTipo.pop();
 
@@ -160,40 +217,41 @@ public class Semantico implements Constants {
 			pilhaTipo.push(ETipo.BOOLEAN.toString());
 			codeAppend("clt");
 		} else {
-			// senão erro semântico, halt
+			throw new SemanticError(
+					"Tipos imcompatíves para relação \"menor que\" (<)");
 		}
 
 	}
 
-	private void acao_09() {
+	private void acao_09() throws SemanticError {
 		String tipo1 = pilhaTipo.pop();
 		String tipo2 = pilhaTipo.pop();
+
 		if (tipo1.equalsIgnoreCase(tipo2)) {
 			pilhaTipo.push(ETipo.BOOLEAN.toString());
 			codeAppend("cgt");
 		} else {
-			// senão erro semântico, halt
+			throw new SemanticError(
+					"Tipos imcompatíves para relação \"maior que\" (>)");
 		}
 	}
 
-	private void acao_10() {
+	private void acao_10() throws SemanticError {
 		String tipo1 = pilhaTipo.pop();
 		String tipo2 = pilhaTipo.pop();
+
 		if (tipo1.equalsIgnoreCase(tipo2)) {
 			pilhaTipo.push(ETipo.BOOLEAN.toString());
 			codeAppend("ceq");
 		} else {
-			// senão erro semântico, halt
+			throw new SemanticError(
+					"Tipos imcompatíves para relação \"igual a\" (=)");
 		}
 	}
 
 	private void acao_11(Token token) {
 		pilhaTipo.push(ETipo.BOOLEAN.toString());
-		if (token.getLexeme().equalsIgnoreCase("true")) {
-			codeAppend("ldc.i4.1");
-		} else {
-			codeAppend("ldc.i4.0");
-		}
+		codeAppend("ldc.i4.1");
 	}
 
 	private void acao_12() {
@@ -201,43 +259,41 @@ public class Semantico implements Constants {
 		codeAppend("ldc.i4.0");
 	}
 
-	private void acao_13() {
+	private void acao_13() throws SemanticError {
 		String tipo = pilhaTipo.pop();
 		if (tipo.equalsIgnoreCase(ETipo.BOOLEAN.toString())) {
 			pilhaTipo.push(ETipo.BOOLEAN.toString());
 			codeAppend("ldc.i4.1");
 			codeAppend("xor");
 		} else {
-			// senão erro semântico, halt
+			throw new SemanticError(
+					"Tipo imcompatível para operação lógica not, esperado tipo boolean");
 		}
 	}
 
 	private void acao_14() {
 		String tipo = pilhaTipo.pop();
-		/*
-		 * if (tipo.equalsIgnoreCase(ETipo.INT.toString())) {
-		 * codigo.append("call void [mscorlib]System.Console::Write(int64)/n");
-		 * } else { if (tipo.equalsIgnoreCase(ETipo.FLOAT.toString())) {
-		 * codigo.append
-		 * ("call void [mscorlib]System.Console::Write(float64)/n"); } else { if
-		 * (tipo.equalsIgnoreCase(ETipo.BOOLEAN.toString())) {
-		 * codigo.append("call void [mscorlib]System.Console::Write(bool)/n"); }
-		 * } }
-		 */
 
-		// Java 7 \0/
 		switch (tipo) {
 		case "int64":
-			codeAppend("call void [mscorlib]System.Console::Write(" + ETipo.INT.getTipoMSIL() + ")");
+			codeAppend("call void [mscorlib]System.Console::Write("
+					+ ETipo.INT.getTipoMSIL() + ")");
 			break;
 		case "float64":
-			codeAppend("call void [mscorlib]System.Console::Write(" + ETipo.FLOAT.getTipoMSIL() + ")");
+			codeAppend("call void [mscorlib]System.Console::Write("
+					+ ETipo.FLOAT.getTipoMSIL() + ")");
 			break;
 		case "bool":
-			codeAppend("call void [mscorlib]System.Console::Write(" + ETipo.BOOLEAN.getTipoMSIL() + ")");
+			codeAppend("call void [mscorlib]System.Console::Write("
+					+ ETipo.BOOLEAN.getTipoMSIL() + ")");
+			break;
+		case "string":
+			codeAppend("call void [mscorlib]System.Console::Write("
+					+ ETipo.STRING.getTipoMSIL() + ")");
 			break;
 		}
-		codeAppend("");
+
+		codeAppend();
 	}
 
 	/**
@@ -248,7 +304,7 @@ public class Semantico implements Constants {
 		codeAppend(".assembly " + nomeArquivo + "{}");
 		codeAppend(".module " + nomeArquivo + ".exe");
 		codeAppend("");
-		codeAppend(".class public _publica{");
+		codeAppend(".class public " + nomeArquivo + " {");
 		codeAppend(".method static public void _principal()");
 		codeAppend("{");
 		codeAppend(".entrypoint");
@@ -265,8 +321,33 @@ public class Semantico implements Constants {
 
 	private void acao_17() {
 		codeAppend("ldstr " + '\"' + "\\n" + '\"');
-		codeAppend("call void [mscorlib]System.Console::Write(" + ETipo.STRING.getTipoMSIL() + ")");
-		codeAppend("");
+		codeAppend("call void [mscorlib]System.Console::Write("
+				+ ETipo.STRING.getTipoMSIL() + ")");
+		codeAppend();
+	}
+
+	private void acao_18() {
+		// TODO implementar
+	}
+
+	private void acao_19() {
+		// TODO implementar
+	}
+
+	private void acao_20() {
+		// TODO implementar
+	}
+
+	private void acao_21() {
+		// TODO implementar
+	}
+
+	private void acao_22() {
+		// TODO implementar
+	}
+
+	private void acao_23() {
+		// TODO implementar
 	}
 
 	private void acao_24(Token token) {
@@ -276,6 +357,12 @@ public class Semantico implements Constants {
 			break;
 		case "real":
 			pilhaTipo.push(ETipo.FLOAT.getTipoMSIL());
+			break;
+		case "string":
+			pilhaTipo.push(ETipo.STRING.getTipoMSIL());
+			break;
+		case "boolean":
+			pilhaTipo.push(ETipo.BOOLEAN.getTipoMSIL());
 			break;
 		}
 	}
@@ -288,26 +375,32 @@ public class Semantico implements Constants {
 		String tipo = pilhaTipo.pop();
 		for (String id : listaIdentificadores) {
 			if (tabelaSimbolo.containsKey(id)) {
-				throw new SemanticError("FUUUUU!!!!");
+				throw new SemanticError("Identificador " + id + " já foi declarado");
 			}
 			tabelaSimbolo.put(id, tipo);
 			codeAppend(".locals (" + tipo + " " + id + ")");
-			codeAppend("");
+			codeAppend();
 		}
 	}
 
 	private void acao_27() throws SemanticError {
 		for (String id : listaIdentificadores) {
 			if (!tabelaSimbolo.containsKey(id)) {
-				throw new SemanticError("FUUUUU!!!!");
+				throw new SemanticError("Identificador " + id + " não declarado");
 			}
-			// FIXME: verificar se não é id de "programa"
+			
+			// FIXME: verificar se não é id de "programa", falta verificar se é isso mesmo
+			if (id.equalsIgnoreCase(nomeArquivo)) {
+				throw new SemanticError(""); //TODO verificar mensagem adequada
+			}
+
 			String tipo = tabelaSimbolo.get(id);
 			codeAppend("call string [mscorlib]System.Console::ReadLine()");
 			if (!tipo.equalsIgnoreCase(ETipo.STRING.toString())) {
-				codeAppend("call " + tipo + " [mscorlib]System.Int64::Parse(string)");
+				codeAppend("call " + tipo
+						+ " [mscorlib]System.Int64::Parse(string)");
 				codeAppend("stloc " + id);
-				codeAppend("");
+				codeAppend();
 			}
 		}
 	}
@@ -315,9 +408,14 @@ public class Semantico implements Constants {
 	private void acao_28(Token token) throws SemanticError {
 		String id = token.getLexeme();
 		if (!tabelaSimbolo.containsKey(id)) {
-			throw new SemanticError("FUUUUU!!!!");
+			throw new SemanticError("Identificador " + id + " não declarado");
 		}
-		// FIXME: verificar se não é id de "programa"
+
+		// FIXME: verificar se não é id de "programa", falta verificar se é isso mesmo
+		if (id.equalsIgnoreCase(nomeArquivo)) {
+			throw new SemanticError(""); //TODO verificar mensagem adequada
+		}
+
 		pilhaTipo.push(tabelaSimbolo.get(id));
 		codeAppend("ldloc " + id);
 	}
@@ -327,7 +425,12 @@ public class Semantico implements Constants {
 		if (!tabelaSimbolo.containsKey(id)) {
 			throw new SemanticError("FUUUUU!!!!");
 		}
-		// FIXME: verificar se não é id de "programa"
+
+		// FIXME: verificar se não é id de "programa", falta verificar se é isso mesmo
+		if (id.equalsIgnoreCase(nomeArquivo)) {
+			throw new SemanticError(""); //TODO verificar mensagem adequada
+		}
+
 		// tipo expressão
 		String tipo1 = pilhaTipo.pop();
 		// tipo do id
@@ -336,7 +439,31 @@ public class Semantico implements Constants {
 			throw new SemanticError("FUUUUU!!!!");
 		}
 		codeAppend("stloc " + id);
-		codeAppend("");
+		codeAppend();
+	}
+
+	private void acao_30() {
+		// TODO implementar
+	}
+
+	private void acao_31() {
+		// TODO implementar
+	}
+
+	private void acao_32() {
+		// TODO implementar
+	}
+
+	private void acao_33() {
+		// TODO implementar
+	}
+
+	private void acao_34() {
+		// TODO implementar
+	}
+
+	private void acao_35() {
+		// TODO implementar
 	}
 
 	private void empilhaInt() {
@@ -363,6 +490,13 @@ public class Semantico implements Constants {
 	 */
 	private void codeAppend(String code) {
 		codigo.append(code);
+		codigo.append("\n");
+	}
+
+	/**
+	 * Adiciona a quebra de linha.
+	 */
+	private void codeAppend() {
 		codigo.append("\n");
 	}
 
